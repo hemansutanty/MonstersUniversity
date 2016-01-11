5d47009cd343b459896e96ea2e92c1971a331f7c
Hemansu Kumar Tanty <hemansu.tanty@gmail.com>
2016-01-11 23:16:30 +0530

    Corrected the variable names and properties file

--- src/main/java/com/hemansu/studentregistration/model/Register.java
+++ src/main/java/com/hemansu/studentregistration/model/Register.java
@@ -35,62 +35,62 @@ public class Register implements Serializable{
 	private String lastNameStudent;
 	@Column(name="adateofbirth")
 	@Temporal(TemporalType.DATE)
-	private Date aDateOfBirth;
+	private Date dateOfBirthStudent;
 	@Column(name="aemail")
-	private String aEmail;
+	private String emailStudent;
 	@Column(name="agender")
-	private String aGender;
+	private String genderStudent;
 	@Column(name="anationality")
-	private String aNationality;
+	private String nationalityStudent;
 	
 	//Address and contact of the applicant attributes
 	@Column(name="aaddressline1")
-	private String aAddressLine1;
+	private String addressLine1Student;
 	@Column(name="aaddressline2")
-	private String aAddressLine2;
+	private String addressLine2Student;
 	@Column(name="acity")
-	private String aCity;
+	private String cityStudent;
 	@Column(name="astate")
-	private String aState;
+	private String stateStudent;
 	@Column(name="acountry")
-	private String aCountry;
+	private String countryStudent;
 	@Column(name="azipcode")
-	private String aZipCode;
+	private String zipCodeStudent;
 	@Column(name="acontactno1")
-	private String aContactNo1;
+	private String contactNo1Student;
 	@Column(name="acontactno2")
-	private String aContactNo2;
+	private String contactNo2Student;
 	
 	//Guardian Information
 	//g is for guardian
 	@Column(name="gguardianname")
-	private String gGuardianName;
+	private String guardianName;
 	@Column(name="gaddressline1")
-	private String gAddressLine1;
+	private String addressLine1Guardian;
 	@Column(name="gaddressline2")
-	private String gAddressLine2;
+	private String addressLine2Guardian;
 	@Column(name="gcity")
-	private String gCity;
+	private String cityGuardian;
 	@Column(name="gstate")
-	private String gState;
+	private String stateGuardian;
 	@Column(name="gcountry")
-	private String gCountry;
+	private String countryGuardian;
 	@Column(name="gzipcode")
-	private String gZipCode;
+	private String zipCodeGuardian;
 	@Column(name="gcontactno1")
-	private String gContactNo1;
+	private String contactNo1Guardian;
 	@Column(name="gcontactno2")
-	private String gContactNo2;
+	private String contactNo2Guardian;
 	
 	//Required for Admission Details
 	@Column(name="arank")
-	private int aRank;
+	private int iitjeeRank;
 	@Column(name="a12thpercentage")
-	private int a12thPercentage;
+	private int percentage12th;
 	@Column(name="a10thpercentage")
-	private int a10thPercentage;
+	private int percentage10th;
 	@Column(name="apassword")
-	private String aPassword;
+	private String password;
 	/**
 	 * @return the firstNameStudent
 	 */
@@ -127,4 +127,317 @@ public class Register implements Serializable{
 	public void setLastNameStudent(String lastNameStudent) {
 		this.lastNameStudent = lastNameStudent;
 	}
+	/**
+	 * @return the dateOfBirthStudent
+	 */
+	public Date getDateOfBirthStudent() {
+		return dateOfBirthStudent;
+	}
+	/**
+	 * @param dateOfBirthStudent the dateOfBirthStudent to set
+	 */
+	public void setDateOfBirthStudent(Date dateOfBirthStudent) {
+		this.dateOfBirthStudent = dateOfBirthStudent;
+	}
+	/**
+	 * @return the emailStudent
+	 */
+	public String getEmailStudent() {
+		return emailStudent;
+	}
+	/**
+	 * @param emailStudent the emailStudent to set
+	 */
+	public void setEmailStudent(String emailStudent) {
+		this.emailStudent = emailStudent;
+	}
+	/**
+	 * @return the genderStudent
+	 */
+	public String getGenderStudent() {
+		return genderStudent;
+	}
+	/**
+	 * @param genderStudent the genderStudent to set
+	 */
+	public void setGenderStudent(String genderStudent) {
+		this.genderStudent = genderStudent;
+	}
+	/**
+	 * @return the nationalityStudent
+	 */
+	public String getNationalityStudent() {
+		return nationalityStudent;
+	}
+	/**
+	 * @param nationalityStudent the nationalityStudent to set
+	 */
+	public void setNationalityStudent(String nationalityStudent) {
+		this.nationalityStudent = nationalityStudent;
+	}
+	/**
+	 * @return the aId
+	 */
+	public int getaId() {
+		return aId;
+	}
+	/**
+	 * @param aId the aId to set
+	 */
+	public void setaId(int aId) {
+		this.aId = aId;
+	}
+	/**
+	 * @return the addressLine1Student
+	 */
+	public String getAddressLine1Student() {
+		return addressLine1Student;
+	}
+	/**
+	 * @param addressLine1Student the addressLine1Student to set
+	 */
+	public void setAddressLine1Student(String addressLine1Student) {
+		this.addressLine1Student = addressLine1Student;
+	}
+	/**
+	 * @return the addressLine2Student
+	 */
+	public String getAddressLine2Student() {
+		return addressLine2Student;
+	}
+	/**
+	 * @param addressLine2Student the addressLine2Student to set
+	 */
+	public void setAddressLine2Student(String addressLine2Student) {
+		this.addressLine2Student = addressLine2Student;
+	}
+	/**
+	 * @return the cityStudent
+	 */
+	public String getCityStudent() {
+		return cityStudent;
+	}
+	/**
+	 * @param cityStudent the cityStudent to set
+	 */
+	public void setCityStudent(String cityStudent) {
+		this.cityStudent = cityStudent;
+	}
+	/**
+	 * @return the stateStudent
+	 */
+	public String getStateStudent() {
+		return stateStudent;
+	}
+	/**
+	 * @param stateStudent the stateStudent to set
+	 */
+	public void setStateStudent(String stateStudent) {
+		this.stateStudent = stateStudent;
+	}
+	/**
+	 * @return the countryStudent
+	 */
+	public String getCountryStudent() {
+		return countryStudent;
+	}
+	/**
+	 * @param countryStudent the countryStudent to set
+	 */
+	public void setCountryStudent(String countryStudent) {
+		this.countryStudent = countryStudent;
+	}
+	/**
+	 * @return the zipCodeStudent
+	 */
+	public String getZipCodeStudent() {
+		return zipCodeStudent;
+	}
+	/**
+	 * @param zipCodeStudent the zipCodeStudent to set
+	 */
+	public void setZipCodeStudent(String zipCodeStudent) {
+		this.zipCodeStudent = zipCodeStudent;
+	}
+	/**
+	 * @return the contactNo1Student
+	 */
+	public String getContactNo1Student() {
+		return contactNo1Student;
+	}
+	/**
+	 * @param contactNo1Student the contactNo1Student to set
+	 */
+	public void setContactNo1Student(String contactNo1Student) {
+		this.contactNo1Student = contactNo1Student;
+	}
+	/**
+	 * @return the contactNo2Student
+	 */
+	public String getContactNo2Student() {
+		return contactNo2Student;
+	}
+	/**
+	 * @param contactNo2Student the contactNo2Student to set
+	 */
+	public void setContactNo2Student(String contactNo2Student) {
+		this.contactNo2Student = contactNo2Student;
+	}
+	/**
+	 * @return the guardianName
+	 */
+	public String getGuardianName() {
+		return guardianName;
+	}
+	/**
+	 * @param guardianName the guardianName to set
+	 */
+	public void setGuardianName(String guardianName) {
+		this.guardianName = guardianName;
+	}
+	/**
+	 * @return the addressLine1Guardian
+	 */
+	public String getAddressLine1Guardian() {
+		return addressLine1Guardian;
+	}
+	/**
+	 * @param addressLine1Guardian the addressLine1Guardian to set
+	 */
+	public void setAddressLine1Guardian(String addressLine1Guardian) {
+		this.addressLine1Guardian = addressLine1Guardian;
+	}
+	/**
+	 * @return the addressLine2Guardian
+	 */
+	public String getAddressLine2Guardian() {
+		return addressLine2Guardian;
+	}
+	/**
+	 * @param addressLine2Guardian the addressLine2Guardian to set
+	 */
+	public void setAddressLine2Guardian(String addressLine2Guardian) {
+		this.addressLine2Guardian = addressLine2Guardian;
+	}
+	/**
+	 * @return the cityGuardian
+	 */
+	public String getCityGuardian() {
+		return cityGuardian;
+	}
+	/**
+	 * @param cityGuardian the cityGuardian to set
+	 */
+	public void setCityGuardian(String cityGuardian) {
+		this.cityGuardian = cityGuardian;
+	}
+	/**
+	 * @return the stateGuardian
+	 */
+	public String getStateGuardian() {
+		return stateGuardian;
+	}
+	/**
+	 * @param stateGuardian the stateGuardian to set
+	 */
+	public void setStateGuardian(String stateGuardian) {
+		this.stateGuardian = stateGuardian;
+	}
+	/**
+	 * @return the countryGuardian
+	 */
+	public String getCountryGuardian() {
+		return countryGuardian;
+	}
+	/**
+	 * @param countryGuardian the countryGuardian to set
+	 */
+	public void setCountryGuardian(String countryGuardian) {
+		this.countryGuardian = countryGuardian;
+	}
+	/**
+	 * @return the zipCodeGuardian
+	 */
+	public String getZipCodeGuardian() {
+		return zipCodeGuardian;
+	}
+	/**
+	 * @param zipCodeGuardian the zipCodeGuardian to set
+	 */
+	public void setZipCodeGuardian(String zipCodeGuardian) {
+		this.zipCodeGuardian = zipCodeGuardian;
+	}
+	/**
+	 * @return the contactNo1Guardian
+	 */
+	public String getContactNo1Guardian() {
+		return contactNo1Guardian;
+	}
+	/**
+	 * @param contactNo1Guardian the contactNo1Guardian to set
+	 */
+	public void setContactNo1Guardian(String contactNo1Guardian) {
+		this.contactNo1Guardian = contactNo1Guardian;
+	}
+	/**
+	 * @return the contactNo2Guardian
+	 */
+	public String getContactNo2Guardian() {
+		return contactNo2Guardian;
+	}
+	/**
+	 * @param contactNo2Guardian the contactNo2Guardian to set
+	 */
+	public void setContactNo2Guardian(String contactNo2Guardian) {
+		this.contactNo2Guardian = contactNo2Guardian;
+	}
+	/**
+	 * @return the iitjeeRank
+	 */
+	public int getIitjeeRank() {
+		return iitjeeRank;
+	}
+	/**
+	 * @param iitjeeRank the iitjeeRank to set
+	 */
+	public void setIitjeeRank(int iitjeeRank) {
+		this.iitjeeRank = iitjeeRank;
+	}
+	/**
+	 * @return the percentage12th
+	 */
+	public int getPercentage12th() {
+		return percentage12th;
+	}
+	/**
+	 * @param percentage12th the percentage12th to set
+	 */
+	public void setPercentage12th(int percentage12th) {
+		this.percentage12th = percentage12th;
+	}
+	/**
+	 * @return the percentage10th
+	 */
+	public int getPercentage10th() {
+		return percentage10th;
+	}
+	/**
+	 * @param percentage10th the percentage10th to set
+	 */
+	public void setPercentage10th(int percentage10th) {
+		this.percentage10th = percentage10th;
+	}
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
 }
--- src/main/webapp/register.jsp
+++ src/main/webapp/register.jsp
@@ -17,50 +17,50 @@
 		<s:textfield key="registerBean.middleNameStudent" ></s:textfield>
 		<s:textfield key="registerBean.lastNameStudent" ></s:textfield>
 		
-		<s:textfield key="registerBean.dateOfBirth" ></s:textfield>
-		<s:textfield key="registerBean.aEmail"></s:textfield>
-		<s:radio key="registerBean.aGender" list="genders" />
-		<s:textfield key="registerBean.aNationality"></s:textfield>
-		<s:textfield key="registerBean.aAddressLine1"></s:textfield>
-		<s:textfield key="registerBean.aAddressLine2"></s:textfield>
-		<s:textfield key="registerBean.aCity"></s:textfield>
-		<s:textfield key="registerBean.aState"></s:textfield>
-		<s:textfield key="registerBean.aCountry"></s:textfield>
-		<s:textfield key="registerBean.aZipCode"></s:textfield>
+		<s:textfield key="registerBean.dateOfBirthStudent" ></s:textfield>
+		<s:textfield key="registerBean.emailStudent"></s:textfield>
+		<s:radio key="registerBean.genderStudent" list="genders" />
+		<s:textfield key="registerBean.nationalityStudent"></s:textfield>
+		<s:textfield key="registerBean.addressLine1Student"></s:textfield>
+		<s:textfield key="registerBean.addressLine2Student"></s:textfield>
+		<s:textfield key="registerBean.cityStudent"></s:textfield>
+		<s:textfield key="registerBean.stateStudent"></s:textfield>
+		<s:textfield key="registerBean.countryStudent"></s:textfield>
+		<s:textfield key="registerBean.zipCodeStudent"></s:textfield>
 		
-		<s:textfield key="registerBean.aContactNo1"></s:textfield>
-		<s:textfield key="registerBean.aContactNo2"></s:textfield>
+		<s:textfield key="registerBean.contactNo1Student"></s:textfield>
+		<s:textfield key="registerBean.contactNo2Student"></s:textfield>
 		
 		<br><br>
 		
 		<s:label for="guardian_information" value="Guardian Information"></s:label>
 		<s:label for="guardianseparator" value="-------------------------"></s:label>
-		<s:textfield key="registerBean.gGuardianName" ></s:textfield>
-		<s:textfield key="registerBean.gAddressLine1"></s:textfield>
-		<s:textfield key="registerBean.gAddressLine2"></s:textfield>
-		<s:textfield key="registerBean.gCity"></s:textfield>
-		<s:textfield key="registerBean.gState"></s:textfield>
-		<s:textfield key="registerBean.gCountry"></s:textfield>
-		<s:textfield key="registerBean.gZipCode"></s:textfield>
+		<s:textfield key="registerBean.guardianName" ></s:textfield>
+		<s:textfield key="registerBean.addressLine1Guardian"></s:textfield>
+		<s:textfield key="registerBean.addressLine2Guardian"></s:textfield>
+		<s:textfield key="registerBean.cityGuardian"></s:textfield>
+		<s:textfield key="registerBean.stateGuardian"></s:textfield>
+		<s:textfield key="registerBean.countryGuardian"></s:textfield>
+		<s:textfield key="registerBean.zipCodeGuardian"></s:textfield>
 		
-		<s:textfield key="registerBean.gContactNo1"></s:textfield>
-		<s:textfield key="registerBean.gContactNo2"></s:textfield>
+		<s:textfield key="registerBean.contactNo1Guardian"></s:textfield>
+		<s:textfield key="registerBean.contactNo2Guardian"></s:textfield>
 		
 		<br><br>
 		
 		<s:label for="admission_details" value="Admission Details"></s:label>
 		<s:label for="admissionseparator" value="-------------------------"></s:label>
 		
-		<s:textfield key="registerBean.aIITJEERank"></s:textfield>
+		<s:textfield key="registerBean.iitjeeRank"></s:textfield>
 		
-		<s:textfield key="registerBean.a12thPercentage"></s:textfield>
-		<s:textfield key="registerBean.a10thPercentage"></s:textfield>
+		<s:textfield key="registerBean.percentage12th"></s:textfield>
+		<s:textfield key="registerBean.percentage10th"></s:textfield>
 		
 		<br><br>
 		
 		<s:label for="login_details" value="Login Details"></s:label>
 		<s:label for="admissionseparator" value="-------------------------"></s:label>
-		<s:textfield key="registerBean.aPassword"></s:textfield>
+		<s:textfield key="registerBean.password"></s:textfield>
 		
 		<s:submit key="submit"></s:submit>
 	</s:form>
